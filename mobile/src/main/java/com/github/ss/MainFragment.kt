e102c6a617a7de351513fa462b03cab13a8439a1
u03013112 <u03013112@hotmail.com>
2019-12-12 17:51:39 +0800

    线路选择界面第一版

--- mobile/src/main/java/com/github/ss/MainFragment.kt
+++ mobile/src/main/java/com/github/ss/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.github.ss
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.github.ss.acl.Acl
 import com.github.ss.bg.BaseService
 import com.github.ss.database.ProfileManager
@@ -16,7 +19,11 @@ import com.github.ss.preference.DataStore
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_j.*
 import kotlinx.android.synthetic.main.activity_j.zhinengshangwang_switch
+import kotlinx.android.synthetic.main.activity_store.*
 import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.layout_line.view.*
+import kotlinx.android.synthetic.main.layout_production.view.*
+import org.jetbrains.anko.longToast
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.support.v4.longToast
 import org.jetbrains.anko.support.v4.toast
@@ -87,16 +94,20 @@ class MainFragment : Fragment() {
                     val d = Gson().fromJson(str, getLineListData::class.java)
                     if (d == null) {
                         toast("d==null")
+                        Log.e("J", "d==null")
                         return@post
                     }
-                    Log.e("J",d.toString())
-                    if (d.error != ""){
+                    if (d.error != null && d.error != ""){
                         longToast(d.error)
+                        Log.e("J", "error:${d.error}")
+                        updateLineButton()
                         return@post
                     }
                     for (line in d.list) {
                         prodectionList.add(line)
                     }
+                    initRecyclerView()
+                    updateLineButton()
                     return@post
                 }, {
             err -> Log.e("J", err)
@@ -109,6 +120,62 @@ class MainFragment : Fragment() {
             line_button.text = "无线路"
             line_button.onClick {
                 getLineList()
+            }
+        }else{
+            line_button.text = "有线路"
+            line_button.onClick {
+                line_list.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private lateinit var lineAdapter : LineAdapter
+    private fun initRecyclerView(){
+        line_list.apply {
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            val topSpacingDecorator = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingDecorator)
+            lineAdapter = LineAdapter()
+            adapter = lineAdapter
+        }
+    }
+    inner class TopSpacingItemDecoration(private val padding: Int) : RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            super.getItemOffsets(outRect, view, parent, state)
+            outRect.top = padding
+        }
+    }
+    inner class LineAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return Holder(
+                    LayoutInflater.from(parent.context).inflate(R.layout.layout_line, parent, false)
+            )
+        }
+
+        override fun getItemCount(): Int {
+            return prodectionList.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            when(holder) {
+                is Holder -> {
+                    holder.bind(prodectionList.get(position))
+                }
+            }
+        }
+
+        inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView),View.OnClickListener {
+            var id = 0
+            fun bind(line : getLineData) {
+                itemView.line_name.setText(line.name)
+                itemView.line_address.setText(line.address)
+                itemView.line_desc.setText(line.description)
+                id = line.id.toInt()
+                itemView.setOnClickListener(this)
+            }
+            override fun onClick(v: View?) {
+                longToast("内测阶段，暂不支持支付")
+                Log.v("J",id.toString())
             }
         }
     }
