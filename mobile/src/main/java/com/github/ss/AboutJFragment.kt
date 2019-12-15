4454b08542830cd271f203b87bd4d29ba26e7509
u03013112 <u03013112@hotmail.com>
2019-12-15 19:57:50 +0800

    关于界面

--- mobile/src/main/java/com/github/ss/AboutJFragment.kt
+++ mobile/src/main/java/com/github/ss/AboutJFragment.kt
@@ -6,12 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_about_j.*
+import kotlinx.android.synthetic.main.layout_webview_dialog.*
 
 class AboutJFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_about_j, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        about_webview.loadUrl("https://frp.u03013112.win:18022/htmls/about.html")
+        about_webview.settings.javaScriptEnabled = true
+        about_webview.loadUrl("https://frp.u03013112.win:18022/htmls/ss-html/#/about")
     }
 }
