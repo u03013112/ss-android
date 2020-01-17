f0ac06cd8390ec1875543673375e4a945bf801f4
u03013112 <u03013112@hotmail.com>
2020-01-17 18:34:17 +0800

    添加tx流量统计，版本更新为v0.0.10

--- mobile/src/main/java/com/github/kxu03013112/JNewActivity.kt
+++ mobile/src/main/java/com/github/kxu03013112/JNewActivity.kt
@@ -39,7 +39,7 @@ import com.umeng.analytics.MobclickAgent
 import com.umeng.commonsdk.UMConfigure
 
 class JNewActivity : AppCompatActivity(), ShadowsocksConnection.Callback, RewardedVideoAdListener {
-    val version : String = "v0.0.5"
+    val version : String = "v0.0.10"
 
     var viewPager: ViewPager? = null
     private var tabLayout: TabLayout? = null
